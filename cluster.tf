6da1db6773a74ad8e455a8b071249d2f760ff2a8
Anmol Nagpal <ianmolnagpal@gmail.com>
2018-06-07 16:34:01 +0400

    update

--- cluster.tf
+++ cluster.tf
@@ -61,3 +61,28 @@ resource "aws_eks_cluster" "eks-cluster" {
     "aws_iam_role_policy_attachment.k8s-cluster-AmazonEKSServicePolicy",
   ]
 }
+
+###___ ____
+##|_ _|  _ \
+##| || |_) |
+##| ||  __/
+#|___|_|
+
+#
+# laptop External IP
+#
+# This configuration is not required and is
+# only provided as an example to easily fetch
+# the external IP of your local laptop to
+# configure inbound EC2 Security Group access
+# to the Kubernetes cluster.
+#
+
+data "http" "laptop-external-ip" {
+  url = "http://icanhazip.com"
+}
+
+# Override with variable or hardcoded value if necessary
+locals {
+  laptop-external-cidr = "${chomp(data.http.laptop-external-ip.body)}/32"
+}
